154e98ce52acdac80eb6321a27bfe9694e5508ac
Júnior Morais <jrmorais3@gmail.com>
2022-07-08 14:45:02 -0300

    Routes creation

---
+++ Shop/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    [Route("Categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return "Get";
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public string GetById(int id)
+        {
+            return "Get" + id.ToString();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public string Post()
+        {
+            return "Post";
+        }
+
+        [HttpPut]
+        [Route("")]
+        public string Put()
+        {
+            return "Put";
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public string Delete()
+        {
+            return "Delete";
+        }
+    }
+}
