d91273b782a36f9999c8ed89284bd8f488928845
Junior Morais <jrmorais3@gmail.com>
2022-07-19 21:39:47 -0300

    Remotion of unused code in HomeController

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -30,26 +30,5 @@ namespace Shop.Controllers
             return Ok(new { message = "Dados configurados" });
 
         }
-
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
